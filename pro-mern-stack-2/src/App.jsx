ecd465133692e73bdaa8f70d6679f5e196184082
Stefan Hristov <shristov22@gmail.com>
2020-05-17 17:36:33 -0700

    modified app.jsx and the Issue Table class to use an initial component state

--- pro-mern-stack-2/src/App.jsx
+++ pro-mern-stack-2/src/App.jsx
@@ -1,4 +1,4 @@
-const issues = [
+const initialIssues = [
     {
         id: 1, status: "New", owner: "Ravan", effort: 5,
         created: new Date("2018-08-15"), due: undefined,
@@ -37,8 +37,12 @@ class IssueFilter extends React.Component {
 }
 
 class IssueTable extends React.Component {
+    constructor() {
+        super();
+        this.state = {issues: initialIssues}
+    }
     render() {
-        const issueRows = issues.map(issue => <IssueRow key={issue.id} issue={issue} />)
+        const issueRows = this.state.issues.map(issue => <IssueRow key={issue.id} issue={issue} />)
         return (
             <table className="bordered-table">
                 <thead>
