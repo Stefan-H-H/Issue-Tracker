3d950084549e60837694b0a6f2bc7aabc10a2e03
Stefan Hristov <shristov22@gmail.com>
2020-05-20 16:12:05 -0700

    revised loadData() in IssueList to be an async function that creates a GraphQL query to fetch data from server

--- pro-mern-stack-2/src/App.jsx
+++ pro-mern-stack-2/src/App.jsx
@@ -1,21 +1,3 @@
-const initialIssues = [
-    {
-        id: 1, status: "New", owner: "Ravan", effort: 5,
-        created: new Date("2018-08-15"), due: undefined,
-        title: "Error in console when clicking Add",
-    },
-    {
-        id: 2, status: "Assigned", owner: "Eddie", effort: 14,
-        created: new Date("2018-08-16"), due: new Date("2018-08-30"),
-        title: "Missing bottom border on panel",
-    }
-];
-
-const sampleIssue = {
-    status: "New", owner: "Pieta",
-    title: "Completion date should be optional",
-};
-
 function IssueRow(props) {
     const issue = props.issue;
     return (
@@ -23,9 +5,9 @@ function IssueRow(props) {
             <td>{issue.id}</td>
             <td>{issue.status}</td>
             <td>{issue.owner}</td>
-            <td>{issue.created.toDateString()}</td>
+            <td>{issue.created}</td>
             <td>{issue.effort}</td>
-            <td>{issue.due ? issue.due.toDateString() : ""}</td>
+            <td>{issue.due}</td>
             <td>{issue.title}</td>
         </tr>
     );
@@ -40,7 +22,7 @@ class IssueFilter extends React.Component {
 }
 
 function IssueTable(props){
-    const issueRows = props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />)
+    const issueRows = props.issues.map(issue => <IssueRow key={issue.id} issue={issue} />);
     return (
         <table className="bordered-table">
             <thead>
@@ -99,10 +81,21 @@ class IssueList extends React.Component {
         this.loadData()
     }
 
-    loadData() {
-        setTimeout(() => {
-            this.setState({issues: initialIssues});
-        }, 500);
+    async loadData() {
+        const query = `query {
+            issueList {
+                id title status owner
+                created effort due
+            }
+        }`;
+
+        const response = await fetch('/graphql', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json'},
+            body: JSON.stringify({ query })
+        });
+        const result = await response.json();
+        this.setState({ issues: result.data.issueList });
     }
 
     createIssue(issue) {
