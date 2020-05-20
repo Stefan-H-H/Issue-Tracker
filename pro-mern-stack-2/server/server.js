c14c611b56e6735d02a489907a61020194b4cf51
Stefan Hristov <shristov22@gmail.com>
2020-05-20 14:58:33 -0700

    initialized About GraphQL About API

--- pro-mern-stack-2/server/server.js
+++ pro-mern-stack-2/server/server.js
@@ -1,4 +1,34 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+
+let aboutMessage = "Issue Tracker API v1.0";
+
+const typeDefs = `
+    type Query {
+        about: String!
+  }
+    type Mutation {
+        setAboutMessage(message: String!): String
+  }
+`;
+
+const resolvers = {
+    Query: {
+        about: () => aboutMessage,
+    },
+    Mutation: {
+        setAboutMessage,
+    },
+  };
+
+function setAboutMessage(_, { message }) {
+    return aboutMessage = message;
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+});
 
 // Creates & initializes an express application.
 const app = express();
@@ -6,6 +36,10 @@ const app = express();
 //express.static()creates a middleware function
 //app.use() method mounts the middleware function
 app.use(express.static('public'));
+
+// Installs the Apollo Server as a middleare in Express
+// server.applyMiddleware({ app, path: '/graphql' });
+server.applyMiddleware({ app, path: '/graphql' });
 
 // listen() method starts server and has it serve HTTP requests.
 // starts teh server and waits eternally for requests.
