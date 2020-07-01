1f8ea247f389598998aa054bfaeefae2430dd0c9
Stefan Hristov <shristov22@gmail.com>
2020-06-30 23:50:53 -0700

    implemented Upate API for editing an issue

--- pro-mern-stack-2/api/issue.js
+++ pro-mern-stack-2/api/issue.js
@@ -7,7 +7,6 @@ async function get(_, { id }) {
   return issue;
 }
 
-
 async function list(_, { status, effortMin, effortMax }) {
   const db = getDb();
   const filter = {};
@@ -50,4 +49,21 @@ async function add(_, { issue }) {
   return savedIssue;
 }
 
-module.exports = { list, add, get };
+async function update(_, { id, changes }) {
+  const db = getDb();
+  if (changes.title || changes.status || changes.owner) {
+    const issue = await db.collection('issues').findOne({ id });
+    Object.assign(issue, changes);
+    validate(issue);
+  }
+  await db.collection('issues').updateOne({ id }, { $set: changes });
+  const savedIssue = await db.collection('issues').findOne({ id });
+  return savedIssue;
+}
+
+module.exports = {
+  list,
+  add,
+  get,
+  update,
+};
