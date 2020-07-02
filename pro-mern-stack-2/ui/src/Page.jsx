aca7c0867feef974682ef3c8e99fff84fc508ed9
Stefan Hristov <shristov22@gmail.com>
2020-07-01 21:52:45 -0700

    Panels- implemented collapsible Filter panel

--- pro-mern-stack-2/ui/src/Page.jsx
+++ pro-mern-stack-2/ui/src/Page.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {
   Navbar, Nav, NavItem, NavDropdown,
   MenuItem, Glyphicon, Tooltip, OverlayTrigger,
+  Grid,
 } from 'react-bootstrap';
 import { LinkContainer } from 'react-router-bootstrap';
 import Contents from './Contents.jsx';
@@ -63,7 +64,9 @@ export default function Page() {
   return (
     <div>
       <NavBar />
-      <Contents />
+      <Grid fluid>
+        <Contents />
+      </Grid>
       <Footer />
     </div>
   );
