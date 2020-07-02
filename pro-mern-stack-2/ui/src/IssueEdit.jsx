91665c371505974244e5f41318538d2586c95f5e
Stefan Hristov <shristov22@gmail.com>
2020-07-02 02:46:13 -0700

    Toasts - replaced previous alerts and errors with Toast components

--- pro-mern-stack-2/ui/src/IssueEdit.jsx
+++ pro-mern-stack-2/ui/src/IssueEdit.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
-import { 
-  Col, Panel, Form, FormGroup, FormControl, ControlLabel, Alert,
-  ButtonToolbar, Button,
- } from 'react-bootstrap';
+import {
+  Col, Panel, Form, FormGroup, FormControl, ControlLabel,
+  ButtonToolbar, Button, Alert,
+} from 'react-bootstrap';
 
 import graphQLFetch from './graphQLFetch.js';
 import NumInput from './NumInput.jsx';
 import DateInput from './DateInput.jsx';
 import TextInput from './TextInput.jsx';
+import Toast from './Toast.jsx';
 
 export default class IssueEdit extends React.Component {
   constructor() {
@@ -18,12 +19,18 @@ export default class IssueEdit extends React.Component {
       issue: {},
       invalidFields: {},
       showingValidation: false,
+      toastVisible: false,
+      toastMessage: '',
+      toastType: 'success',
     };
     this.onChange = this.onChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
     this.onValidityChange = this.onValidityChange.bind(this);
-    this.dismissValidation =this.dismissValidation.bind(this);
+    this.dismissValidation = this.dismissValidation.bind(this);
     this.showValidation = this.showValidation.bind(this);
+    this.showSuccess = this.showSuccess.bind(this);
+    this.showError = this.showError.bind(this);
+    this.dismissToast = this.dismissToast.bind(this);
   }
 
   componentDidMount() {
@@ -41,7 +48,7 @@ export default class IssueEdit extends React.Component {
   onChange(event, naturalValue) {
     const { name, value: textValue } = event.target;
     const value = naturalValue === undefined ? textValue : naturalValue;
-    this.setState((prevState) => ({
+    this.setState(prevState => ({
       issue: { ...prevState.issue, [name]: value },
     }));
   }
@@ -75,10 +82,10 @@ export default class IssueEdit extends React.Component {
     }`;
 
     const { id, created, ...changes } = issue;
-    const data = await graphQLFetch(query, { changes, id });
+    const data = await graphQLFetch(query, { changes, id }, this.showError);
     if (data) {
       this.setState({ issue: data.issueUpdate });
-      alert('Updated issue successfully'); // eslint-disable-line no-alert
+      this.showSuccess('Updated issue successfully');
     }
   }
 
@@ -91,7 +98,7 @@ export default class IssueEdit extends React.Component {
     }`;
 
     const { match: { params: { id } } } = this.props;
-    const data = await graphQLFetch(query, { id: parseInt(id, 10) });
+    const data = await graphQLFetch(query, { id: parseInt(id, 10) }, this.showEror);
     this.setState({ issue: data ? data.issue : {}, invalidFields: {} });
   }
 
@@ -101,6 +108,22 @@ export default class IssueEdit extends React.Component {
 
   dismissValidation() {
     this.setState({ showingValidation: false });
+  }
+
+  showSuccess(message) {
+    this.setState({
+      toastVisible: true, toastMessage: message, toastType: 'success',
+    });
+  }
+
+  showError(message) {
+    this.setState({
+      toastVisible: true, toastMessage: message, toastType: 'danger',
+    });
+  }
+
+  dismissToast() {
+    this.setState({ toastVisible: false });
   }
 
   render() {
@@ -123,9 +146,10 @@ export default class IssueEdit extends React.Component {
       );
     }
 
-    const { issue: { title, status }, } = this.state;
+    const { issue: { title, status } } = this.state;
     const { issue: { owner, effort, description } } = this.state;
     const { issue: { created, due } } = this.state;
+    const { toastVisible, toastMessage, toastType } = this.state;
 
     return (
       <Panel>
@@ -247,6 +271,13 @@ export default class IssueEdit extends React.Component {
           {' | '}
           <Link to={`/edit/${id + 1}`}>Next</Link>
         </Panel.Footer>
+        <Toast
+          showing={toastVisible}
+          onDismiss={this.dismissToast}
+          bsStyle={toastType}
+        >
+          {toastMessage}
+        </Toast>
       </Panel>
     );
   }
